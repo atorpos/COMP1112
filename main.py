257aa1c0fcaaed0f2995911d6f27c0186701c497
Oskar Wong <atorpos@mac.com>
2023-09-29 20:37:22 -0400

    Assignment 1

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+#Student Name: Sun Fai Oskar Wong
+#Student Number: 200561852
+#Course Number: COMP1112-23F-13374
+#assignment 1
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def print_the_star(row):
+    for i in range(1, row+1):
+        for j in range(row, i, -1):
+            print(" ", end="")
+        for k in range(1, i + 1):
+            print("*", end=" ")
+        print()
+    print()
 
-# Press the green button in the gutter to run the script.
+def question_ans(input_number):
+    formula_text = ""
+    sum_total = 0
+    for i in range(1, input_number+1):
+        sum_total = sum_total + i
+        if formula_text == "":
+            formula_text = str(i)
+        else:
+            formula_text = formula_text + " + " + str(i)
+    print(formula_text + " = " +str(sum_total))
+    print()
+
+def question_3(input_char):
+    letter_count = len(input_char)
+    char_list = []
+    for char in input_char:
+        char_list.append(char)
+    for i in range(0, letter_count):
+        if i %2 != 1:
+            print(f"The char {i+1} is {char_list[i]}")
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('this is Oskar')
+n = 5
+print('Question 1')
+print_the_star(n)
+print('Question 2')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+number_input = input("Please input your number:")
+question_ans(int(number_input))
+
+print('Question 3')
+question_3_input = input("Please input word: ")
+question_3(question_3_input)
+
